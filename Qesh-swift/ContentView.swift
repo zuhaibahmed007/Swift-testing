7a3f7a6fa59ed94ec31e8c6ec4786e1be00513d4
Amir Khan <amir-khanzada@iinv.tech>
2023-11-30 12:59:08 +0500

    Initial Commit

---
+++ Qesh-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Qesh-swift
+//
+//  Created by Amir Khan on 30/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Qesh-swift/Qesh_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qesh_swiftApp.swift
+//  Qesh-swift
+//
+//  Created by Amir Khan on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Qesh_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
